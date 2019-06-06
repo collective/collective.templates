30d24114267f89c95f148750fdfb434d323982a0
andreasma <maand@gmx.de>
2019-06-06 21:26:41 +0200

    further code fixes.

--- src/collective/templates/tlcenter.py
+++ src/collective/templates/tlcenter.py
@@ -21,12 +21,12 @@ MULTISPACE = u'\u3000'.encode('utf-8')
 BAD_CHARS = ('?', '-', '+', '*', MULTISPACE)
 
 checkEmail = re.compile(
-    r"[a-zA-Z0-9._%-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,4}").match
+    r'[a-zA-Z0-9._%-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,4}').match
 
 
 def validateEmail(value):
     if not checkEmail(value):
-        raise Invalid(_(u"Invalid email address"))
+        raise Invalid(_(u'Invalid email address'))
     return True
 
 
@@ -35,34 +35,34 @@ class ITLCenter(model.Schema):
     """
 
     title = schema.TextLine(
-        title=_(u"Name Of The Templates Center"),
+        title=_(u'Name Of The Templates Center'),
     )
 
     description = schema.Text(
-        title=_(u"Description of the Templates Center"),
+        title=_(u'Description of the Templates Center'),
     )
 
     product_description = schema.Text(
-        title=_(u"Description of the features of templates"),
+        title=_(u'Description of the features of templates'),
     )
 
     product_title = schema.TextLine(
-        title=_(u"Template Product Name"),
+        title=_(u'Template Product Name'),
         description=_(
-            u"Name of the Template product, e.g. only Templates"),
+            u'Name of the Template product, e.g. only Templates'),
     )
 
     model.fieldset('categories_et_all',
-                   label=u"Categories et all",
+                   label=u'Categories et all',
                    fields=['available_category', 'available_licenses',
                            'available_versions', 'available_platforms'])
 
-    available_category = schema.List(title=_(u"Available Categories"),
+    available_category = schema.List(title=_(u'Available Categories'),
                                      default=['Business',
                                               ],
                                      value_type=schema.TextLine())
 
-    available_licenses = schema.List(title=_(u"Available Licenses"),
+    available_licenses = schema.List(title=_(u'Available Licenses'),
                                      default=[
                                          'GNU-GPL-v2 (GNU General Public '
                                          'License Version 2)',
@@ -82,15 +82,15 @@ class ITLCenter(model.Schema):
                                          'CC-BY-SA-v4 (Creative Commons '
                                          'Attribution-ShareAlike 4.0 '
                                          'International)',
-                                         'AL-v2 (Apache License Version 2.0)',],
+                                         'AL-v2 (Apache License Version 2.0)', ],
                                      value_type=schema.TextLine())
 
-    available_versions = schema.List(title=_(u"Available Versions"),
+    available_versions = schema.List(title=_(u'Available Versions'),
                                      default=['Product 1.0',
-                                             ],
+                                              ],
                                      value_type=schema.TextLine())
 
-    available_platforms = schema.List(title=_(u"Available Platforms"),
+    available_platforms = schema.List(title=_(u'Available Platforms'),
                                       default=['All platforms',
                                                'Linux',
                                                'Linux-x64',
@@ -106,14 +106,14 @@ class ITLCenter(model.Schema):
 
     primary('install_instructions')
     install_instructions = RichText(
-        title=_(u"Template Installation Instructions"),
-        description=_(u"Please fill in the install instructions"),
+        title=_(u'Template Installation Instructions'),
+        description=_(u'Please fill in the install instructions'),
         required=False
     )
 
     primary('reporting_bugs')
     reporting_bugs = RichText(
-        title=_(u"Instruction how to report Bugs"),
+        title=_(u'Instruction how to report Bugs'),
         required=False
     )
 
@@ -124,46 +124,46 @@ class ITLCenter(model.Schema):
                            'legal_downloaddisclaimer'])
 
     title_legaldisclaimer = schema.TextLine(
-        title=_(u"Title for Legal Disclaimer and Limitations"),
-        default=_(u"Legal Disclaimer and Limitations"),
+        title=_(u'Title for Legal Disclaimer and Limitations'),
+        default=_(u'Legal Disclaimer and Limitations'),
         required=False
     )
 
     legal_disclaimer = schema.Text(
-        title=_(u"Text of the Legal Disclaimer and Limitations"),
+        title=_(u'Text of the Legal Disclaimer and Limitations'),
         description=_(
-            u"Enter the text of the legal disclaimer and limitations that "
-            u"should be displayed to the project creator and should be "
-            u"accepted by the owner of the project."),
+            u'Enter the text of the legal disclaimer and limitations that '
+            u'should be displayed to the project creator and should be '
+            u'accepted by the owner of the project.'),
         default=_(
-            u"Fill in the legal disclaimer, that had to be accepted by the "
-            u"project owner"),
+            u'Fill in the legal disclaimer, that had to be accepted by the '
+            u'project owner'),
         required=False
     )
 
     title_legaldownloaddisclaimer = schema.TextLine(
         title=_(
-            u"Title of the Legal Disclaimer and Limitations for Downloads"),
-        default=_(u"Legal Disclaimer And Limitations For Downloads"),
+            u'Title of the Legal Disclaimer and Limitations for Downloads'),
+        default=_(u'Legal Disclaimer And Limitations For Downloads'),
         required=False
     )
 
     primary('legal_downloaddisclaimer')
     legal_downloaddisclaimer = RichText(
-        title=_(u"Text of the Legal Disclaimer and Limitations for Downlaods"),
+        title=_(u'Text of the Legal Disclaimer and Limitations for Downlaods'),
         description=_(
-            u"Enter any legal disclaimer and limitations for downloads that "
-            u"should appear on each page for dowloadable files."),
-        default=_(u"Fill in the text for the legal download disclaimer"),
+            u'Enter any legal disclaimer and limitations for downloads that '
+            u'should appear on each page for dowloadable files.'),
+        default=_(u'Fill in the text for the legal download disclaimer'),
         required=False
     )
 
     primary('information_oldversions')
     information_oldversions = RichText(
-        title=_(u"Information About Search For Old Product Versions"),
-        description=_(u"Enter an information about the search for older "
-                      u"versions of the product, if they are not on the "
-                      u"versions list (compatibility) anymore."),
+        title=_(u'Information About Search For Old Product Versions'),
+        description=_(u'Enter an information about the search for older '
+                      u'versions of the product, if they are not on the '
+                      u'versions list (compatibility) anymore.'),
         required=False
     )
 
@@ -173,11 +173,11 @@ class ITLCenter(model.Schema):
 
     contactForCenter = schema.ASCIILine(
         title=_(
-            u"EMail address for communication with the template center "
-            u"manager and reviewer"),
+            u'EMail address for communication with the template center '
+            u'manager and reviewer'),
         description=_(
-            u"Enter an email address for the communication with template "
-            u"center manager and reviewer"),
+            u'Enter an email address for the communication with template '
+            u'center manager and reviewer'),
         default='projects@foo.org',
         constraint=validateEmail
     )
@@ -252,7 +252,7 @@ class TLCenterView(BrowserView):
             'SearchableText': SearchableText,
             'sort_order': 'reverse',
             'portal_type': 'collective.templates.tlproject'
-            }
+        }
 
         if version != 'any':
             contentFilter['getCompatibility'] = version
@@ -265,20 +265,17 @@ class TLCenterView(BrowserView):
         except ParseError:
             return []
 
-
     def munge_search_term(self, q):
         for char in BAD_CHARS:
             char = str(char)
             q = q.replace(char, ' ')
         r = q.split()
-        r = " AND ".join(r)
+        r = ' AND '.join(r)
         r = quote_chars(r) + '*'
         return r
 
-
     def show_search_form(self):
         return 'getCategories' in self.request.environ['QUERY_STRING']
-
 
 
 class TLCenterOwnProjectsViewlet(ViewletBase):
