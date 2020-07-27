d18f85c3ccea7cecf2a79ca038d250f10c84f13f
andreasma <maand@gmx.de>
2020-07-27 15:41:55 +0200

    Add new vocabularies module.

---
+++ src/collective/templates/vocabularies.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from plone.app.vocabularies.terms import safe_simplevocabulary_from_values
+from zope.interface import provider
+from zope.schema.interfaces import IVocabularyFactory
+
+
+@provider(IVocabularyFactory)
+def CategoriesVocabularyFactory(context):
+    values = api.portal.get_registry_record('collectivetemplates.available_category')
+    return safe_simplevocabulary_from_values(values)
+
+
+@provider(IVocabularyFactory)
+def LicensesVocabularyFactory(context):
+    values = api.portal.get_registry_record('collectivetemplates.available_licenses')
+    return safe_simplevocabulary_from_values(values)
+
+
+@provider(IVocabularyFactory)
+def VersionsVocabularyFactory(context):
+    values = api.portal.get_registry_record('collectivetemplates.available_versions')
+    return safe_simplevocabulary_from_values(values)
+
+
+@provider(IVocabularyFactory)
+def PlatformVocabularyFactory(context):
+    values = api.portal.get_registry_record('collectivetemplates.available_platforms')
+    return safe_simplevocabulary_from_values(values)
