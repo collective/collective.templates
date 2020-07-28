6b3b692790915735b3096f5f7104ecdd8bdba59d
andreasma <maand@gmx.de>
2020-07-28 15:14:20 +0200

    Remove unused fields.

--- src/collective/templates/tlcenter.py
+++ src/collective/templates/tlcenter.py
@@ -4,7 +4,6 @@ from collective.templates import _
 from collective.templates.common import validateemail
 from plone import api
 from plone.app.layout.viewlets import ViewletBase
-from plone.app.multilingual.dx import directives
 from plone.app.textfield import RichText
 from plone.supermodel import model
 from plone.supermodel.directives import primary
@@ -41,75 +40,6 @@ class ITLCenter(model.Schema):
             'Name of the Template product, e.g. only Templates')),
     )
 
-    model.fieldset('categories_et_all',
-                   label=_(safe_unicode('Categories et all')),
-                   fields=['available_category', 'available_licenses',
-                           'available_versions', 'available_platforms'])
-
-    available_category = schema.List(title=_(safe_unicode('Available Categories')),
-                                     default=['Business',
-                                              ],
-                                     value_type=schema.TextLine())
-
-    available_licenses = schema.List(title=_(safe_unicode('Available Licenses')),
-                                     default=[
-                                         'GNU-GPL-v2 (GNU General Public'
-                                         'License Version 2)',
-                                         'GNU-GPL-v3+ (General Public License'
-                                         'Version 3 and later)',
-                                         'LGPL-v2.1 (GNU Lesser General'
-                                         'Public License Version 2.1)',
-                                         'LGPL-v3+ (GNU Lesser General Public'
-                                         'License Version 3 and later)',
-                                         'BSD (BSD License (revised))',
-                                         'MPL-v1.1 (Mozilla Public License'
-                                         'Version 1.1)',
-                                         'MPL-v2.0+ (Mozilla Public License'
-                                         'Version 2.0 or later)',
-                                         'CC-by-sa-v3 (Creative Commons'
-                                         'Attribution-ShareAlike 3.0)',
-                                         'CC-BY-SA-v4 (Creative Commons'
-                                         'Attribution-ShareAlike 4.0 '
-                                         'International)',
-                                         'AL-v2 (Apache License Version 2.0)'],
-                                     value_type=schema.TextLine())
-
-    available_versions = schema.List(title=_(safe_unicode('Available Versions')),
-                                     default=['Product 1.0',
-                                              ],
-                                     value_type=schema.TextLine())
-
-    available_platforms = schema.List(title=_(safe_unicode('Available Platforms')),
-                                      default=['All platforms',
-                                               'Linux',
-                                               'Linux-x64',
-                                               'Mac OS X',
-                                               'Windows',
-                                               'BSD',
-                                               'UNIX (other)'],
-                                      value_type=schema.TextLine())
-
-    model.fieldset('allowedfileextensions',
-                   label=_(safe_unicode('Allowed file extensions')),
-                   fields=['allowed_fileextension',
-                           'allowed_imageextension',
-                           ],
-                   )
-
-    allowed_fileextension = schema.TextLine(
-        title=_(safe_unicode('Allowed file extensions')),
-        description=_(safe_unicode(
-            'Fill in the allowed file extensions, seperated by '
-            "a pipe '|'.")),
-    )
-
-    allowed_imageextension = schema.TextLine(
-        title=_(safe_unicode('Allowed image file extension')),
-        description=_(safe_unicode(
-            'Fill in the allowed image file extensions, seperated '
-            "by a pipe '|'.")),
-    )
-
     model.fieldset('instructions',
                    label=_(safe_unicode('Instructions')),
                    fields=['install_instructions',
@@ -133,27 +63,8 @@ class ITLCenter(model.Schema):
 
     model.fieldset('disclaimer',
                    label=_(safe_unicode('Legal Disclaimer')),
-                   fields=['title_legaldisclaimer', 'legal_disclaimer',
-                           'title_legaldownloaddisclaimer',
+                   fields=['title_legaldownloaddisclaimer',
                            'legal_downloaddisclaimer'])
-
-    title_legaldisclaimer = schema.TextLine(
-        title=_(safe_unicode('Title for Legal Disclaimer and Limitations')),
-        default=_(safe_unicode('Legal Disclaimer and Limitations')),
-        required=False,
-    )
-
-    legal_disclaimer = schema.Text(
-        title=_(safe_unicode('Text of the Legal Disclaimer and Limitations')),
-        description=_(safe_unicode(
-            'Enter the text of the legal disclaimer and limitations that '
-            'should be displayed to the project creator and should be '
-            'accepted by the owner of the project.')),
-        default=_(safe_unicode(
-            'Fill in the legal disclaimer, that had to be accepted by the '
-            'project owner')),
-        required=False,
-    )
 
     title_legaldownloaddisclaimer = schema.TextLine(
         title=_(safe_unicode(
@@ -196,12 +107,6 @@ class ITLCenter(model.Schema):
         default='projects@foo.org',
         constraint=validateemail,
     )
-
-
-directives.languageindependent('available_category')
-directives.languageindependent('available_licenses')
-directives.languageindependent('available_versions')
-directives.languageindependent('available_platforms')
 
 
 class TLCenterView(BrowserView):
