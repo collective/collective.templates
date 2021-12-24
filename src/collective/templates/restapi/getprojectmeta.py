6f9d91d514840d925880bc19477d3f4f5f399194
andreasma <maand@gmx.de>
2021-12-24 15:52:28 +0100

    Add restapi endpoint for getting licenses idx data of a template project from the portal_catalog.

--- src/collective/templates/restapi/getprojectmeta.py
+++ src/collective/templates/restapi/getprojectmeta.py
@@ -5,10 +5,18 @@ from plone.restapi.serializer.converters import json_compatible
 
 
 class Projectcategories(Service):
-
     def reply(self):
         catalog = api.portal.get_tool(name='portal_catalog')
         path = '/'.join(self.context.getPhysicalPath())
         idx_data = catalog.getIndexDataForUID(path)
         category = idx_data.get('getCategories')
         return json_compatible(category)
+
+
+class Projectlicenses(Service):
+    def reply(self):
+        catalog = api.portal.get_tool(name='portal_catalog')
+        path = '/'.join(self.context.getPhysicalPath())
+        idx_data = catalog.getIndexDataForUID(path)
+        licenses = idx_data.get('releaseLicense')
+        return json_compatible(licenses)
